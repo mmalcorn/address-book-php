13afadd010881a89b0ea158c1db6861cc6df5065
Meredith Alcorn <alcornmeredith@gmail.com>
2016-09-09 07:24:52 -0700

    initialize a session, create a class constructor for contacts, and add getter and setter functions for private properties of the contact object

--- app/app.php
+++ app/app.php
@@ -6,6 +6,10 @@
     $app->register(new Silex\Provider\TwigServiceProvider(),
       array('twig.path'=>__DIR__.'/../views'));
 
+    session_start();
+      if(empty($_SESSION['contact-list'])){
+        $_SESSION['contact-list'] = array();
+      }
 
     return $app
  ?>
--- src/Contact.php
+++ src/Contact.php
@@ -1,7 +1,40 @@
 <?php
   Class Contact {
+    private $name;
+    private $phone_number;
+    private $address;
 
-  }
+
+    function __construct($new_contact_info){
+      $this->name;
+      $this->phone_number;
+      $this->address;
+    }
+
+    function getName(){
+      $this->name;
+    }
+
+    function setName($new_name){
+      $this->name = $new_name;
+    }
+
+    function getNumber(){
+      $this->phone_number;
+    }
+
+    function setNumber($new_number){
+      $this->phone_number = $new_number;
+    }
+
+    function getAddress(){
+      $this->address;
+    }
+
+    function setAddress($new_address){
+      $this->address = $new_address;
+    }
+}
 
 
  ?>
